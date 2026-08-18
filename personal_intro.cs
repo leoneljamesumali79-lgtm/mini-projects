6dfd09781903fbab66a2052bdbbcfadced488b05
leoneljamesumali79-lgtm <leoneljamesumali79@gmail.com>
2026-08-18 17:36:01 +0800

    Add files via upload

---
+++ personal_intro.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace PersonalIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Leonel Umali";
+            int age = 19;
+            string program = "BS Information Technology";   
+
+            Console.WriteLine($"Hello! My name is {name}. I am {age} years old and I am currently enrolled in the {program} program.");
+            Console.WriteLine($"I am {age} years old.");
+            Console.WriteLine($"I am taking {program}.");
+        }
+    }
+}
