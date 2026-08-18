35285dac1e943cf3267de185ae73f9ffd708ab0e
leoneljamesumali79-lgtm <leoneljamesumali79@gmail.com>
2026-08-18 17:40:31 +0800

    Add files via upload

---
+++ access_string.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace AccessString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string string_word = "PROGRAMMING";
+
+            Console.WriteLine($"First Character: {string_word[0]}");
+            Console.WriteLine($"Third character: {string_word[2]}");
+            Console.WriteLine($"Last character: {string_word[10]}");
+        
+        }
+    }
+}
