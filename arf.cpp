273034c994b9a3395a26fd3da783076eac26a500
leoneljamesumali79-lgtm <leoneljamesumali79@gmail.com>
2026-08-19 10:37:24 +0800

    Add files via upload

---
+++ arf.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Leonel James D. Umali \n";
+    cout << "19";
+    return 0;
+}
---
+++ student_report_card.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int studentID = 2502281;
+    double score = 90.5;
+    char grade = 'A';
+
+    cout << "Student ID: " << studentID << endl;
+    cout << "Score: " << score << endl;
+    cout << "Grade: " << grade << endl;
+
+    return 0;
+}
