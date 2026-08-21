c0c46daf5b3a3074ad5cea3150e2c5f582746907
leoneljamesumali79-lgtm <leoneljamesumali79@gmail.com>
2026-08-21 17:31:43 +0800

    Add files via upload
    Create a program that asks for a student's grade.
    
    Determine whether the statement "The student passed" is true or false.
    
    A passing grade is 75 or higher.

---
+++ passing_a_subject.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int grade;
+    cout << "Enter your grade: ";
+    cin >> grade;
+
+    cout << "\nPassed: " << (grade >= 75) << endl;
+
+}
